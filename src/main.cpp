4b3c393a925dabcd0175a9f7f91c5fc0bd1e6eca
Mark Braithwaite <mark.braith@gmail.com>
2020-07-15 20:46:36 +0100

    Definitions and presetup headers created.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,19 @@
+/* ------------ STJORN controller --------------
+ *  Code written for Teensy 3.2 micro-controller
+ *  Teensy USB Type should be set to 'MIDI' 
+ * 
+ * https://github.com/markbrai/stjorn 
+ * https://hackaday.io/project/162616-stjrn
+ * 
+ * https://www.gigperformer.com
+ * 
+ * Mark Braithwaite, 2020
+ * 
+ */
+
 #include <Arduino.h>
+#include "definitions.h"
+#include "presetup.h"
 
 void setup() {
   // put your setup code here, to run once:
