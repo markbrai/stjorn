bb758f99c091c867e81eff3cc2e386318800c5bd
Mark Braithwaite <mark.braith@gmail.com>
2020-07-15 21:03:24 +0100

    Added basic setup to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,54 @@
  */
 
 #include <Arduino.h>
-#include "definitions.h"
-#include "presetup.h"
+#include "definitions.h"    // holds all STJORN definitions
+#include "devices.h"        // holds instances of all STJORN devices
 
 void setup() {
-  // put your setup code here, to run once:
+
+// TURN POWER LED ON
+  pinMode(PIN_PWRLED, OUTPUT);
+  digitalWrite(PIN_PWRLED, HIGH);
+
+// DISPLAY SETUP
+// NOTE: displays should be started before TWIST. For some reason...
+// start displays
+  display1.begin(0x70);
+  display2.begin(0x71);
+  display3.begin(0x72);
+
+// set display brightness
+  display1.setBrightness(SCRN_DIM);
+  display2.setBrightness(SCRN_DIM);
+  display3.setBrightness(SCRN_DIM);
+
+// setup welcome message
+  display1.writeDigitAscii(3,'S');
+  display2.writeDigitAscii(0,'T');
+  display2.writeDigitAscii(1,'J');
+  display2.writeDigitAscii(2,'O');
+  display2.writeDigitAscii(3,'R');
+  display3.writeDigitAscii(0,'N');
+
+// write displays
+  display1.writeDisplay();
+  display2.writeDisplay();
+  display3.writeDisplay();
+
+// TWIST SETUP
+  twist.begin();
+
+// LED SETUP
+  leds.begin();
+  leds.setBrightness(LED_DIM);
+  for(int i = 0; i < NUM_LEDS; i++) {
+      leds.setPixel(i,BLUE);
+  }
+  leds.show();
+
+// SETUP RELAY
+  pinMode(PIN_RELAY, OUTPUT);
+    
 }
 
 void loop() {
