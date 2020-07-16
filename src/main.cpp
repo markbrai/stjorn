24abf52568877f48a91a50e144d70ae76088ce09
Mark Braithwaite <mark.braith@gmail.com>
2020-07-16 20:35:23 +0100

    Device definitions moved back in to main.cpp to avoid multiple definition errors

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,37 @@
 
 #include <Arduino.h>
 #include "STJORN_definitions.h"    // holds all STJORN definitions
-#include "STJORN_devices.h"        // holds instances of all STJORN devices
+//#include "STJORN_devices.h"        // holds instances of all STJORN devices
+#include <SPI.h>                                    // required for PlatformIO build...
+#include "SparkFun_Qwiic_Twist_Arduino_Library.h"   // for rotary encoder
+#include <Adafruit_GFX.h>                           // for quad alphanumeric
+#include "Adafruit_LEDBackpack.h"                   // for quad alphanumeric
+#include <WS2812Serial.h>                           // for neoPixels
+#include <Wire.h>                                   // for i2c comms
+#include <Bounce2.h>                                // for button debounce
+#include "Adafruit_VCNL4010.h"                      // for proximity sensor
+
+// Instantiate encoder
+TWIST twist;
+
+// Instantiate displays
+Adafruit_AlphaNum4 display1 = Adafruit_AlphaNum4();     // left display
+Adafruit_AlphaNum4 display2 = Adafruit_AlphaNum4();     // centre display
+Adafruit_AlphaNum4 display3 = Adafruit_AlphaNum4();     // right display
+
+// Instantiate WS2812Serial for LEDs
+byte drawingMemory[NUM_LEDS*3];
+DMAMEM byte displayMemory[NUM_LEDS*12];
+
+WS2812Serial leds(NUM_LEDS, displayMemory, drawingMemory, PIN_WS2812, WS2812_RGB);
+
+// Instantiate button debouncers
+const uint8_t BUTTON_PINS[NUM_BUTTONS] = {23,22,21,20,17,16,15,14,33,32,31,30,28,27,26};
+Bounce *buttons = new Bounce[NUM_BUTTONS];
+
+// Instantiate proximity sensor
+Adafruit_VCNL4010 vcnl;
+
 
 void setup() {
 
@@ -53,13 +83,17 @@ void setup() {
   leds.begin();
   leds.setBrightness(LED_DIM);
   for(int i = 0; i < NUM_LEDS; i++) {
-      leds.setPixel(i,BLUE);
+      leds.setPixel(i,RED);
   }
   leds.show();
 
 // SETUP RELAY
   pinMode(PIN_RELAY, OUTPUT);
-    
+
+
+delay(5000);
+
+
 }
 
 
@@ -84,5 +118,11 @@ void loop() {
    * Update screens as required
    * Update current state
    */
+
+  for(int i = 0; i < NUM_LEDS; i++) {
+      leds.setPixel(i,DARK);
+  }
+  leds.show();
+
 
 }
