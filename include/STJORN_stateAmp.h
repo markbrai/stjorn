d1d92ed020c93e96eca9cdccd0166ed3753fad3a
Mark Braithwaite <mark.braithwaite@ricardo.com>
2024-02-09 12:00:31 +0000

    Created files for amp patch select

---
+++ include/STJORN_stateAmp.h
@@ -0,0 +1,25 @@
+/* ------------ STJORN controller --------------
+ *  Code written for Teensy 3.2 micro-controller
+ *  Teensy USB Type should be set to 'MIDI'
+ *
+ * https://github.com/markbrai/stjorn
+ * https://hackaday.io/project/162616-stjrn
+ *
+ * https://www.gigperformer.com
+ *
+ * Mark Braithwaite, 2020
+ *
+ */
+
+/* ------------ STATE - AMP --------------
+ *  Functions for selecting amp base patches
+ *  ---------------------------------------- */
+
+#ifndef STJORN_STATEAMP_H
+#define STJORN_STATEAMP_H
+
+#include <Arduino.h>
+#include <Bounce2.h>
+#include "STJORN_definitions.h"
+
+#endif
